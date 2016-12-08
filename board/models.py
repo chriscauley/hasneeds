ad47dfdfc66ac3589073aa9501aaf8545620132a
chriscauley <chris@lablackey.com>
2016-12-08 17:41:09 -0500

    initial models

--- board/models.py
+++ board/models.py
@@ -1,5 +1,42 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=64,unique=True)
+  __unicode__ = lambda self: self.name
+  class Meta:
+    ordering = ("name",)
+
+class TagManager(models.Manager):
+  def get(self,*args,**kwargs):
+    if 'name' in kwargs:
+      kwargs['name'] = slugify(kwargs['name'])
+    return super(TagManager,self).get(*args,**kwargs)
+
+class Tag(models.Model):
+  name = models.CharField(max_length=64,unique=True)
+  tags = models.ManyToManyField(Tag)
+  _ht = "Non-approved tags may be deleted if they aren't up to standards."
+  approved = models.BooleanField(default=False,help_text=_ht)
+  __unicode__ = lambda self: self.name
+  def save(self,*args,**kwargs):
+    self.name = slugify(self.name)
+    super(Tag,self).save(*args,**kwargs)
+  class Meta:
+    ordering = ("name",)
+
+class Post(modelsModel):
+  name = models.CharField(max_length=256)
+  tags = models.ManyToManyField(Tag)
+  categories = models.ManyToManyField(Category)
+  user = models.ForeignKey(user=settings.AUTH_USER_MODEL)
+  created = models.DateTimeField(auto_now_add=True)
+  modified = models.DateTimeField(auto_now=True)
+  closed = models.DateTimeField(null=True,blank=True)
+  data = jsonfield.JSONField(default={})
+  __unicode__ = lambda self: self.name
+  class Meta:
+    ordering = ("-created",)
