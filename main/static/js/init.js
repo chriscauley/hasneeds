8790c57b4de178a1a3f6e1747172422258932748
chriscauley <chris@lablackey.com>
2016-12-10 11:31:15 -0500

    adding zepto

--- main/static/js/init.js
+++ main/static/js/init.js
@@ -1,3 +1,7 @@
 uR.auth.login_url = "/login/slack/";
 uR.auth.login_icon = "fa fa-slack";
 uR.auth.login_text = "Connect with Slack";
+
+uR.addRoutes({
+  "/": function() { uR.mountElement("post-list"); }
+});
