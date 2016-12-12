cd6bac194b4ebbe7747e0bd91644f417d7b33b89
chriscauley <chris@lablackey.com>
2016-12-12 00:10:15 -0500

    add post view and as_json

--- board/views.py
+++ board/views.py
@@ -1,12 +1,20 @@
 from django.http import JsonResponse
 from django.template.defaultfilters import slugify
 
-from .models import Tag
+from .models import Tag,Post
 
 def tags(request):
   tags = Tag.objects.filter(name__icontains=slugify(request.GET['q']))
-  return JsonResponse([{ 'id': t.id,'name':  t.name } for t in tags],safe=False)
+  return JsonResponse([t.as_json for t in tags],safe=False)
 
 def add_tag(request):
   t,new = Tag.objects.get_or_create(name=request.POST['name'])
-  return JsonResponse({'id': t.id, 'name': t.name})
+  return JsonResponse(t.as_json)
+
+def add_post(request):
+  data = {k:request.POST[k] for k in ['categories','tags','name','description'] if k in request.POST}
+  post = Post.objects.create(
+    user=request.user,
+    **data
+  )
+  return JsonResponse({'ur_route_to': post.get_absoluet_url()})
